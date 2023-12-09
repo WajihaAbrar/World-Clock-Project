3a7f463ca769541f9f86fb1c6cbab0da83e51fa9
WajihaAbrar <wajihaabrar@gmail.com>
2023-12-09 11:29:15 +0000

    Added the date and time functionality in JS file.

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  //Los Angles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let currentLosAngelesElement = moment().tz("America/Los_Angeles");
+  losAngelesDateElement.innerHTML =
+    currentLosAngelesElement.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = currentLosAngelesElement.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
 
+  //Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let currentParisElement = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = currentParisElement.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = currentParisElement.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+updateTime();
+setInterval(updateTime, 1000);
